8fe406967f45a111543d93d8516c53fd000bf2cb
UPDOT\soura <sourab@updot.in>
2023-05-27 08:56:10 +0530

    INIT

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// 1. Return the list of robos with image, name, weapon, healthPoints aligned in one line
+// 2. Create a search input field which will take the user input and filter the robos with names including the search terms.
+// 3. If in case no robo found on search then return "NO ROBO FOUND" message.
+
+import './App.css';
+import roboData from "./robos.json";
+
+function App() {
+  return (
+    <div className="App">
+      <h5 className='text-center font-bold'>Hello from REACT</h5>
+      {/* Code Here */}
+    </div>
+  );
+}
+
+export default App;
